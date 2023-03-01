bca1ce9f464d2c088d7d9c77085582658c26fe47
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-01 12:42:53 -0500

    add POST routes to create a new review

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 
 
-// Create new comment
+// Create new review
 router.post('/', async (req, res) => {
   try {
-    const dbPostData = await Post.create({
-      title: req.body.title,
-      content: req.body.content,
+    const dbReviewData = await Review.create({
+      country_id: req.body.country_id,
       user_id: req.session.user_id,
+      content: req.body.content,
+      rating: req.body.rating,
     });
     res.status(200).json(dbPostData);
   } catch (err) {
