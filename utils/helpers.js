990a5baf432b95e99ee9b95ac9eeb93acb806060
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-01 22:58:18 -0500

    Add function of converting rating to stars ⭐︎

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,14 @@ module.exports = {
   format_date: (date) => { // Format date information to MM/DD/YYYY
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
   },
+  rating_stars: (rating) => { // Convert rating (number) into starts ★
+    stars ="";
+    for (i = 0; i <= rating - 1 ; i++) {
+      stars += "★";
+    }
+    for (j = rating; j < 5 ; j++) {
+      stars += "☆";
+    }
+    return stars
+  },
 };
